cf24770c6a3163eecc5ffbd212e0e8b8e59c4aff
Bob <petrbobe@seznam.cz>
2024-03-02 19:54:29 +0100

    Přidejte soubory projektu.

---
+++ BoardGameStats/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BoardGameStats
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
